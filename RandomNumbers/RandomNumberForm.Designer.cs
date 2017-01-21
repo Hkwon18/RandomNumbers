c4d0afe63ffbf15f8a2f7c2ee9f22e55707971df
Hyuk Jin Kwon <Hyuk Jin Kwon>
2017-01-21 00:53:47 -0800

    First commit

---
+++ RandomNumbers/RandomNumberForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace RandomNumbers
+{
+    partial class RandomNumbersForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(12, 174);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(75, 23);
+            this.GenerateButton.TabIndex = 0;
+            this.GenerateButton.Text = "Generate Numbers";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.Location = new System.Drawing.Point(13, 127);
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.Size = new System.Drawing.Size(259, 20);
+            this.ResultTextBox.TabIndex = 2;
+            this.ResultTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // RandomNumbersForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ResultTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.GenerateButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "RandomNumbersForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Random Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox ResultTextBox;
+    }
+}
+
---
+++ RandomNumbers/RandomNumberForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumbers
+{
+    public partial class RandomNumbersForm : Form
+    {
+        public RandomNumbersForm()
+        {
+            InitializeComponent();
+        }
+
+        public static void FisherYatesShuffle(List<int> orderedList)
+        {
+            Random rng = new Random();
+            int n = orderedList.Count;
+            while (n > 0)
+            {
+                int k = rng.Next(n);
+                n--;
+                int temp = orderedList[n];
+                orderedList[n] = orderedList[k];
+                orderedList[k] = temp;
+            }
+        }
+
+        private static void PrintList(List<int> randomList)
+        {
+            string outputPath = Path.Combine(Directory.GetCurrentDirectory(), "RandomList.txt");
+            TextWriter tw = new StreamWriter(outputPath);
+            foreach (var num in randomList)
+                tw.WriteLine(num.ToString());
+            tw.Close();
+        }
+
+        private void GenerateClick(object sender, EventArgs e)
+        {
+            List<int> randomList = Enumerable.Range(1, 10000).ToList();
+            FisherYatesShuffle(randomList);
+            PrintList(randomList);
+        }
+
+    }
+}
